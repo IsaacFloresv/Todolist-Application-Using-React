0410f5e9ffde12c34981e70856ebd93f7d7d64e7
ISFlores <isaacfloresv@gmail.com>
2022-07-13 18:28:51 +0000

    Visual lista

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,15 +5,40 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<p className="hoja"></p>
-			<p className="hoja1"></p>
-			<p className="hoja2"></p>
-			<p className="hoja3"></p>
-			<p className="hoja4"></p>
-		</div>
-	);
+  return (
+    <div className="text-center mt-5">
+      <div className="container text-center p-1 m-1 hoja">
+        <div className="input-group w-75">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Recipient"
+			readonly
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-secondary"
+              type="button"
+              id="button-addon2"
+            >
+              Button
+            </button>
+          </div>
+        </div>
+        <ul className="list-group">
+          <li className="list-group-item Anch">An item</li>
+          <li className="list-group-item Anch">A second item</li>
+          <li className="list-group-item Anch">A third item</li>
+          <li className="list-group-item Anch">A fourth item</li>
+          <li className="list-group-item Anch">And a fifth one</li>
+        </ul>
+      </div>
+      <div className="hoja1"></div>
+      <div className="hoja2"></div>
+      <div className="hoja3"></div>
+      <div className="hoja4"></div>
+    </div>
+  );
 };
 
 export default Home;
